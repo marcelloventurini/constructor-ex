42a168b08437ddf64b6955029e46f27c821093ae
Marcello Venturini <75899197+marcelloventurini@users.noreply.github.com>
2023-05-19 16:34:22 -0300

    Add files via upload

---
+++ BankRegister/Account.cs
@@ -0,0 +1,41 @@
+﻿namespace BankRegister
+{
+    internal class Account
+    {
+        public string AccountNumber { get; private set; }
+        public string AccountHolder { get; set; }
+        public double Balance { get; private set; }
+
+        public Account(string accountNumber, string accountHolder)
+        {
+            AccountNumber = accountNumber;
+            AccountHolder = accountHolder;
+        }
+
+        public Account(string accountNumber, string accountHolder, double initialDeposit) : this(accountNumber, accountHolder)
+        {
+            Deposit(initialDeposit);
+        }
+
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public void Withdraw(double value)
+        {
+            Balance = (Balance - value) - 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Dados da conta: "
+                + "Nro.: "
+                + AccountNumber
+                + "; Titular: "
+                + AccountHolder
+                + "; Saldo: R$"
+                + Balance.ToString("F2");
+        }
+    }
+}
---
+++ BankRegister/Program.cs
@@ -0,0 +1,39 @@
+﻿using BankRegister;
+
+Account account;
+
+Console.Write("Informe o número da conta: ");
+string accountNumber = Console.ReadLine();
+
+Console.Write("Informe o títular da conta: ");
+string accountHolder = Console.ReadLine();
+
+Console.Write("Gostaria de realizar um depósito inicial? (s/n): ");
+char answer = char.Parse(Console.ReadLine());
+if (answer == 's')
+{
+    Console.Write("Informe o valor do depósito inicial: ");
+    double initialDeposit = double.Parse(Console.ReadLine());
+
+    account = new Account(accountNumber, accountHolder, initialDeposit);
+}
+else
+{
+    account = new Account(accountNumber, accountHolder);
+}
+
+Console.WriteLine(account);
+
+Console.Write("Informe um valor para depósito: ");
+double deposit = double.Parse(Console.ReadLine());
+account.Deposit(deposit);
+
+Console.WriteLine("Dados atualizados:");
+Console.WriteLine(account);
+
+Console.Write("Informe um valor para saque: ");
+double withdraw = double.Parse(Console.ReadLine());
+account.Withdraw(withdraw);
+
+Console.WriteLine("Dados atualizados:");
+Console.WriteLine(account);
